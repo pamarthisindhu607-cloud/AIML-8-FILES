2178aad7acdd0e027d661673c86e592eb5e78f08
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:34:24 +0530

    Add files via upload

---
+++ convert to meter and kilometer.cpp
@@ -0,0 +1,13 @@
+////programme to input length in centimeters and convert it to meter and kilometer
+#include<stdio.h>
+main()
+{
+	float cm,meters,kilometers;
+	printf("enter the length in cm:cm");
+	scanf("%f",fcm);
+	meters=cm/100-0;
+	kilometers =cm/100000 o;
+	printf("length in kilometers:%2fm/n", meters);
+	printf("length in kilometers:%5fkm/h",kilometer);
+}
+
