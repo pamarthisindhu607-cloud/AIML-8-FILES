9bc4c4a1fb8d6b211f237aaf72159c078d39b0db
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:48:19 +0530

    Add files via upload

---
+++ programme to key board.cpp
@@ -0,0 +1,17 @@
+//program to enter array element from key board
+#include <stdio.h>
+main()
+{
+	int i;
+	int marks[6];
+	printf("enter array elements");
+	for(i=0;i<6;i++)
+	{
+		scanf("%d"&marks[i]);
+		printf("the array elements are:\n");
+		for(i=0;i,b;i++)
+	{
+		printf("%d\t",mark[i]);
+	}
+
+}
