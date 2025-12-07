aac7e22f1656bdae5630920601840c358999c13e
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:55:56 +0530

    Add files via upload

---
+++ with argu with returntype.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int addition (int n);
+main()
+{
+	  int n, i, 
+	  printf("enter n value");
+	  scanf("%d%d",&n);
+	  c=addition (n);
+	  printf("the result is %d",c);
+}
+       
+       int addition (int x,int y)
+       {
+       printf("the sum of %d numbers is %d",n,sum);
+       return x+y;
+       }
