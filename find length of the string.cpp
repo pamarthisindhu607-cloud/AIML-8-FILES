8f934f767b30a4f4fe0c6eed905e8c25cf359e59
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:36:05 +0530

    Add files via upload

---
+++ find length of the string.cpp
@@ -0,0 +1,15 @@
+//programme to find length of the stringe
+#include<stdio.h>
+int main()
+{
+	char myster[20];
+	int i,len=0;
+	printf("enter a string");
+	gets(myster);
+	for(i=0;myster[i]!='\0';i++)
+	{
+		len=len+1;
+	}
+	printf("the length of the string is %d",len);
+	return 0;
+}
