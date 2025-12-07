2959c54aadf5f483190cf9e2aa2060f5e13fc819
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:41:41 +0530

    Add files via upload

---
+++ principle,time,and rate final simple interest.cpp
@@ -0,0 +1,12 @@
+//program to input principle,time,and,rate(p t r) from user and final simple interest
+#include<stdio.h>
+main()
+{
+	float principle,time,rate,simple interest;
+	printf("enter principal amount,principal");
+	scanf("%f",&principle;
+	printf("enter time (in years)",time));
+	scanf("%f",&rate);
+	simple interest=principal*time*rate/100;
+	printf("simple interest=%2f/n",simple interest);
+}
