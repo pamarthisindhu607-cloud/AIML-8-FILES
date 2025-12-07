7f1bbef68e0306c5011e29d685bb11c285427cbd
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:39:20 +0530

    Add files via upload

---
+++ maximum and minimum using loop or condition.cpp
@@ -0,0 +1,12 @@
+//programme to find maximum and minimum of two numbers without using any loop or condition
+#include<stdio.h>
+main()
+{
+	int a,b,maximum,minimum;
+	printf("enter two integers:a,b");
+	scanf("%d%d",&a,&b);
+	maximum=(a+b+abs(a-b)/2;
+	minimum(a+b-abs(a-b))/2;
+	printf("maximum=%d/n",maximum);
+	printf("maximum=%d/n",minimum),
+}
