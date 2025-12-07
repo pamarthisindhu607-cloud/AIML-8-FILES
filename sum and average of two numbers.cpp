451e754ecad1b4e4e652eeffbe2a07c9ba914859
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:50:58 +0530

    Add files via upload

---
+++ sum and average of two numbers.cpp
@@ -0,0 +1,13 @@
+//programme to read two integers and find the sum and average of two numbers
+#include<stdio.h>
+main()
+{
+	int num1,num2,num;
+	float average;
+	printf("enter two integers,num1,num2");
+	scanf("%d%d",&Num1,&num2);
+	sum=num1+num2;
+	average=sum/20;
+	printf("average=%2f",average);
+}
+
