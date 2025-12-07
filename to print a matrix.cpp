e5be62843ce2f07c5e601f3acf4db265580a4223
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:55:25 +0530

    Add files via upload

---
+++ to print a matrix.cpp
@@ -0,0 +1,20 @@
+//program to find sum and percentage of marks
+#include<stdio.h>
+main()
+{
+	int i,marks(b);
+	int percentage,sum=0;
+	for(i=0;i<6;i++)
+	{
+		printf("enter %d marks, i+1 marks");
+		scanf("%d",&marks[i]);
+	}
+	   for(i=0;i<b;i++)
+	   {
+	   	sum=sum+marks[i];
+	   }
+	      percentage =(sum/600)*100;
+	      printf("the total marks:%d\n", sum);
+	      printf("the percentage:%d",percentage);
+	  }
+}
