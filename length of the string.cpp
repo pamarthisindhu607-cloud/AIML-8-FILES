0aa1ce86d3262112df6def30bf183359ad4dd33d
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:30:40 +0530

    Add files via upload

---
+++ length of the string.cpp
@@ -0,0 +1,10 @@
+//programme to find length of string
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char s1[20];
+	printf("enter the string");
+	gets(s1);
+	printf("the length of the string is%d",strlen(s1));
+}
