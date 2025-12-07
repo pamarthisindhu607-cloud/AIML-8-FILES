0fdeccc836964d20a1b148ef5e1c8472ca15ec78
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:53:29 +0530

    Add files via upload

---
+++ swap two numbers third variable.cpp
@@ -0,0 +1,15 @@
+//programme to swap two numbers using third variable
+#include<stdio.h>
+main()
+{
+	int a,b,temp;
+	printf("enter two number a,b");
+	scanf("%d%d",&a,&b);
+	printf("before swapping:a=%d,b=%d/n",a,b);
+	temp=a;
+	a=b
+	temp;
+	printf("after swapping:a=%d,b=%d/n",a,b);
+}
+	
+
