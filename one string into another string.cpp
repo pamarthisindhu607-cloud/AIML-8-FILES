49d767516f9650660514d554c25f573eb2fe6dfb
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:40:06 +0530

    Add files via upload

---
+++ one string into another string.cpp
@@ -0,0 +1,17 @@
+//programme to copy one string into another string
+#include<stdio.h>
+main()
+{
+	char s1[20],s2[20];
+	int i=0;
+	printf("enter string 1");
+	gets(s1);
+	while(s1[i]!='\0')
+	{
+		s2[i]=s1[i];
+		i++;
+	}
+	    s2[i]='\0';
+	    printf("the copied string is:");
+	    puts(s2);
+}
