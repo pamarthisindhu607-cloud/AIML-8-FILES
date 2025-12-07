6f9e7b96edbf5467a196a1f0949562a3af1ce93d
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:43:25 +0530

    Add files via upload

---
+++ program on string upper and string lower.cpp
@@ -0,0 +1,10 @@
+//program on strupr()&strlwr()
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char s1[20]="aditya";
+	char s2[20]="university";
+	printf("the aditya in upper case is",strupr(s1));
+	printf("the UNIVERSITY in lower case is", strlwr(s2));
+}
