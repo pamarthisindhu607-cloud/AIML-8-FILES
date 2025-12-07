31152a30f8d858cae3e91b4abf3e9001760f3f25
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:37:11 +0530

    Add files via upload

---
+++ input two numbers find their power.cpp
@@ -0,0 +1,12 @@
+//programme to input two numbers from user and find their power
+#include<stdio.h>
+main()
+{
+	double base,exponent,result;
+	printf("enter base numbers:base");
+	scanf("%if",&base);
+	printf("enter exponent number",exponent);
+	scanf("%f",&exponent);
+	result =(base,exponent);
+	printf("%21f^%21f=%21f/n",base,exponent,result);
+}
