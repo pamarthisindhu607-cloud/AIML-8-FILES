ff5f091a4ce4d462c5f0ca119de2d5a90849e3f5
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 19:03:22 +0530

    Add files via upload

---
+++ c4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,temp;
+	printf("enter two number a,b");
+	scanf("%d%d",&a,);
+	printf("before swapping:a=%d,b=%d/n",a,);
+	
+	printf("after swapping:a=%d,b=%d/n",a,);
+}
+	
+
