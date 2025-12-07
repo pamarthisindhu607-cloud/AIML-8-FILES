6ec8d7b06e9de655adb2430e023f120f8971a5a0
pamarthisindhu607-cloud <pamarthisindhu607@gmail.com>
2025-12-07 18:50:34 +0530

    Add files via upload

---
+++ stringe completely  from user.cpp
@@ -0,0 +1,13 @@
+//programme  to read a stringe completely from user
+#include<stdio.h>
+main()
+{
+	char name[20];
+	printf("enter your name");
+	  gets(name);
+	  printf("my name is :\n");
+	  puts(name);
+	
+	  
+	  
+}
